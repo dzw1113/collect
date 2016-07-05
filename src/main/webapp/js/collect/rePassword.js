f4cc8486b7840daf378150ef0298f54180c4cc19
dzw1113 <649095437@qq.com>
2016-07-05 16:13:05 +0800

    主版本

--- src/main/webapp/js/collect/rePassword.js
+++
@@ -1,7 +0,0 @@
-function usPasswordModify(serviceCode, formName) {
-	sendService(serviceCode, formName, usLoginCallBack);
-}
-
-function usLoginCallBack(responseHeader, responseBody) {
-	layer.alert(responseHeader.returnDesc);
-}
--- src/main/webapp/js/datetimepicker/dirdatatimepicker.js
+++
@@ -1,5 +0,0 @@
-(function(){
-    var datetimepicker=angular.module("datetimepicker",[])
-
-})()
-
